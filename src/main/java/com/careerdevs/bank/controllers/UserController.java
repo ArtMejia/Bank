4fbd900c051fef3f8a497371a404daf9c1411193
ArtMejia <arteniomejia.student@careerdevs.com>
2022-11-17 20:27:32 -0500

    Created User Entity, User Repository and User Controller. Tested routes using logintoken as param

--- src/main/java/com/careerdevs/bank/controllers/UserController.java
+++ src/main/java/com/careerdevs/bank/controllers/UserController.java
@@ -9,8 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.nio.charset.Charset;
-import java.util.List;
+
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
